9c3e83fb73a15791ae86b3086d90b1cb337dd51b
TomekGrzybek <t.grzybowski94@gmail.com>
2025-02-16 22:24:39 +0100

    Homepage placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import styles from "./page.module.scss";
+import Links from "@/Mock/Links";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>Placeholder for now</main>
+      <div className={styles.container}>
+        <h1 className={styles.header}>Homepage Placeholder</h1>
+        {Links.slice(1).map((link) => (
+          <Link key={link.id} href={link.path}>
+            <div className={styles.box}>
+              <link.icon size={200} />
+              <span>{link.name}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
